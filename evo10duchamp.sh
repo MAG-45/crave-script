8600f31a50da4dbade69675b194c5f89f955aa0b
MAG <raphvmag@ik.me>
2025-02-21 13:58:05 +0100

    Update script

--- evo10duchamp.sh
+++ evo10duchamp.sh
@@ -18,18 +18,20 @@ sed -i '/<project path="hardware\/lineage\/compat" name="LineageOS\/android_hard
 rm -rf hardware/lineage/compat/
 echo "Patch lineage manifest"
 
-# Copy singins keys 
-cp keys/* vendor/evolution-priv/keys/
-echo "Copy keys"
-
 # Patch FP Sensors
 cd frameworks/base
 git fetch https://github.com/xiaomi-mt6897-duchamp/android_frameworks_base_new
-git cherry-pick 1d372ceec9c401d2521fb5d51284a8d2e309b6c0 1f4591d4818b77c732963e16175776e09a981c6d
+git cherry-pick -Xtheirs 1d372ceec9c401d2521fb5d51284a8d2e309b6c0 1f4591d4818b77c732963e16175776e09a981c6d
 cd ../..
 
 # Crave sync 
 /opt/crave/resync.sh
+
+# Copy singins keys 
+mkdir vendor/evolution-priv/keys/ -p
+cp keys/* vendor/evolution-priv/keys/
+ln -s build/make/target/product/security/BUILD.bazel vendor/evolution-priv/keys/BUILD.bazel
+echo "Copy keys"
 
 # Start building
 source build/envsetup.sh
