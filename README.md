c464320ff81eaa042a8ee63a1862e45193702eb9
MAG <raphvmag@ik.me>
2025-02-21 11:35:26 +0100

    Add README

---
+++ README.md
@@ -0,0 +1,9 @@
+### Before exect the script 
+
+- Use Crave devspace 
+- Upload your privates keys to keys/
+- And exect it with curl !
+
+```bash
+curl https://raw.githubusercontent.com/MAG-45/crave-script/refs/heads/master/evo10duchamp.sh | sh
+```
--- evo10duchamp.sh
+++ evo10duchamp.sh
@@ -2,32 +2,37 @@
 
 # Remove old local manifests
 rm -rf .repo/local_manifests/
+echo "Remove local manifests"
 
 # Init ROM manifest
 repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
+echo "Init EvoX Manifest"
 
 # Download duchamp manifests
 git clone https://github.com/MAG-45/evox_manifest_duchamp .repo/local_manifests
+echo "Clone manifests for duchamp"
 
 # Patch EvolutionX manifest
 sed -i '/<project path="hardware\/lineage\/compat" name="LineageOS\/android_hardware_lineage_compat" \/>/d' .repo/manifests/snippets/lineage.xml
-
-# Copy singins keys 
-cp keys/* vendor/evolution-priv/keys
-
 # Remove problematic tree 
 rm -rf hardware/lineage/compat/
+echo "Patch lineage manifest"
+
+# Copy singins keys 
+cp keys/* vendor/evolution-priv/keys/
+echo "Copy keys"
 
 # Patch FP Sensors
 cd frameworks/base
-curl -s https://raw.githubusercontent.com/xiaomi-mt6897-duchamp/patches/refs/heads/main/0001-biometrics-virtualhal-Revert-for-mfp-daemon-to-work.patch -s | git am
+git fetch https://github.com/xiaomi-mt6897-duchamp/android_frameworks_base_new
+git cherry-pick 1d372ceec9c401d2521fb5d51284a8d2e309b6c0 1f4591d4818b77c732963e16175776e09a981c6d
 cd ../..
 
 # Crave sync 
 /opt/crave/resync.sh
 
 # Start building
-. build/envsetup.sh
+source build/envsetup.sh
 
 lunch lineage_duchamp-apa4-userdebug
 
